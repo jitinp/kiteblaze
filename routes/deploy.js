69d042b8bed5ab741a90cf90ab11bb16fa060462
jitinp <jitinp@nityaalabs.com>
2017-10-23 00:34:58 -0400

    Added functionality for Process Step progress in {url}/deploy. Javascript issues pending

--- routes/deploy.js
+++ routes/deploy.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET about page. */
 router.get('/', function(req, res, next) {
-  res.render('test', { title: 'Kiteblaze | Deploy apps automatically', page: 'about' });
+  res.render('deploy', { title: 'Kiteblaze | Deploy apps automatically', page: 'about' });
 });
 
 module.exports = router;
