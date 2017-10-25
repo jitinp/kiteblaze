51efa17d90dc50f7522b442abf0eb694093fe3c4
jitinp <jitinp@nityaalabs.com>
2017-10-25 09:18:40 -0400

    Form field to pass GIT Repo link

--- routes/deploy.js
+++ routes/deploy.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('deploy', { title: 'Kiteblaze | Deploy apps automatically', page: 'about' });
 });
 
+router.post('/code', function(req, res, next) {
+	console.log("Git repo link: " + req.body.repolink);
+	return;
+})
+
 module.exports = router;
